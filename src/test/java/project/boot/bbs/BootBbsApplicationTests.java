cf3d1935c0d6072de6ea6443cac2a85928a12914
TTimotti <ttimotti@kakao.com>
2023-05-16 10:44:29 +0900

    2023-05-16 프로젝트 생성

---
+++ src/test/java/project/boot/bbs/BootBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.boot.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
