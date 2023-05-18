4c6096c60d7659a2cbd053ba832e6a7d4c5127d9
TTimotti <ttimotti@kakao.com>
2023-05-18 16:16:11 +0900

    계정 관련 기능 추가

--- src/main/java/project/boot/bbs/controller/UserController.java
+++ src/main/java/project/boot/bbs/controller/UserController.java
@@ -3,6 +3,7 @@ package project.boot.bbs.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
@@ -31,7 +32,7 @@ public class UserController {
 	}
 	
 	@PostMapping(value = "/signUp/insert")
-	public String signUpInsert(SignUpDTO dto) {
+	public String signUpInsert(@RequestBody SignUpDTO dto) {
 		log.info("signUpDto={}", dto);
 		userService.insertUser(dto);
 		
--- src/main/java/project/boot/bbs/dto/SignUpDTO.java
+++ src/main/java/project/boot/bbs/dto/SignUpDTO.java
@@ -1,18 +1,16 @@
 package project.boot.bbs.dto;
 
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 import project.boot.bbs.entity.UserEntity;
 
 @Getter
 @ToString
-@AllArgsConstructor
 public class SignUpDTO {
 	private String userId;
 	private String userName;
-	private String password;	
+	private String password;
 	
 	public UserEntity toEntity() {		
 		
